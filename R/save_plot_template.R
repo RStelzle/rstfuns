c896b0d0969c5f7754f1fd1accb3da06a798c7e7
RStelzle <robertmas98@zedat.fu-berlin.de>
2020-09-30 03:40:55 +0200

    add save_plot_template.R

---
+++ R/save_plot_template.R
@@ -0,0 +1,36 @@
+#' Exporting Plots for use in LaTeX
+#'
+#'
+#' @param plot_object Plot to save. Defaults to last_plot()
+#' @param filenames name of the .tex and .pdf files
+#' @param pdfwidth Width of the .pdf file in inches
+#' @param pdfheight Height of the .pdf file in inches
+#' @param latexwidth Proportion of the linewidth to use in LaTeX. Defaults to 1
+#' @param latexlabel LaTeX label for referencing the plot in the LaTeX document
+#' @param latexcaption Figure Caption in LaTeX
+#' @keywords strings string utility
+#' @export
+
+
+
+
+
+
+save_plot_template <- function(plot_object = ggplot2::last_plot(), filenames, pdfwidth, pdfheight, latexwidth=1, latexlabel, latexcaption) {
+
+  grDevices::cairo_pdf(filename = here::here("output", "figures", paste0(filenames, ".pdf")), width = pdfwidth, height = pdfheight)
+
+  print(plot_object)
+
+  grDevices::dev.off()
+
+  tmptbbl <-c("\\begin{figure}[!t]",
+              "\\centering",
+              paste0("\\includegraphics[width=", latexwidth, "]{../output/figures/", filenames, ".pdf}"),
+              paste0("\\caption{", latexcaption, "\\label{", latexlabel, "}}"),
+              "\\end{figure}"
+  )
+
+  readr::write_lines(tmptbbl, here::here("output", "figures", paste0(filenames, ".tex")))
+
+}
